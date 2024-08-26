c5beb1dd51017a13dff2d8322995fa006e1eeae6
Matthew Read <matthew.read.101@gmail.com>
2024-08-26 16:11:20 -0700

    Added ToDo fragment
    -File renaming to organized a sudo folder structure

---
+++ MultiPurposeApp/app/src/main/java/com/github/shadeslayer1467/ui/todoapp/AddNewTask.java
@@ -0,0 +1,116 @@
+package com.github.shadeslayer1467.ui.todoapp;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import com.github.shadeslayer1467.R;
+import com.github.shadeslayer1467.ui.todoapp.models.ToDoModel;
+import com.github.shadeslayer1467.ui.todoapp.utils.DatabaseHandler;
+
+public class AddNewTask extends BottomSheetDialogFragment {
+
+    public static final String TAG = "ActionBottomDialog";
+
+    private EditText newTaskText;
+    private Button newTaskSaveButton;
+    private DatabaseHandler db;
+
+    public static AddNewTask newInstance(){
+        return new AddNewTask();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NORMAL, R.style.DialogStyle);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_to_do_app_new_task, container, false);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        newTaskText = getView().findViewById(R.id.newTaskText);
+        newTaskSaveButton = getView().findViewById(R.id.newTaskButton);
+
+        db = new DatabaseHandler(getActivity());
+        db.openDatabase();
+
+        boolean isUpdate = false;
+        final Bundle bundle = getArguments();
+        if(bundle != null){
+            isUpdate = true;
+            String task = bundle.getString("task");
+            newTaskText.setText(task);
+            if(!task.isEmpty())
+                newTaskSaveButton.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+        }
+
+
+        newTaskText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().isEmpty()){
+                    newTaskSaveButton.setEnabled(false);
+                    newTaskSaveButton.setTextColor(Color.GRAY);
+                } else {
+                    newTaskSaveButton.setEnabled(true);
+                    newTaskSaveButton.setTextColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        boolean finalIsUpdate = isUpdate;
+        newTaskSaveButton.setOnClickListener(view1 -> {
+            String text = newTaskText.getText().toString();
+            if(finalIsUpdate){
+                db.updateTask(bundle.getInt("id"), text);
+            } else {
+                ToDoModel task = new ToDoModel();
+                task.setTask(text);
+                task.setStatus(0);
+                db.insertTask(task);
+            }
+            dismiss();
+        });
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Fragment navHostFragment = getActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
+        if (navHostFragment != null) {
+            Fragment currentFragment = navHostFragment.getChildFragmentManager().getFragments().get(0);
+            if (currentFragment instanceof DialogCloseListener) {
+                ((DialogCloseListener) currentFragment).handleDialogClose(dialog);
+            }
+        }
+    }
+}
