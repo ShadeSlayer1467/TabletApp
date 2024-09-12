622deb7e30a27fffe71b48ac8bdd17525a27eba8
Matthew Read <matthew.read.101@gmail.com>
2024-09-12 14:54:58 -0700

    trying to persist across navigation

--- MultiPurposeApp/app/src/main/java/com/github/shadeslayer1467/ui/clock/StopwatchFragment.java
+++ MultiPurposeApp/app/src/main/java/com/github/shadeslayer1467/ui/clock/StopwatchFragment.java
@@ -3,9 +3,8 @@ package com.github.shadeslayer1467.ui.clock;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,18 +19,18 @@ import com.github.shadeslayer1467.R;
 import java.util.ArrayList;
 
 public class StopwatchFragment extends Fragment {
-    TextView stopwatchTextView;
-    MaterialButton resetStopwatch, startStopwatch, stopStopwatch;
-    StopwatchManager stopwatchManager;
+    private TextView stopwatchTextView;
+    private MaterialButton resetStopwatch, startStopwatch, stopStopwatch;
+    private StopwatchManager stopwatchManager;
 
-    ListView lvTimes;
-    ArrayList<String> timeList = new ArrayList<>();
-    ArrayAdapter<String> timeListAdapter;
+    private ListView lvTimes;
+    private ArrayList<String> timeList = new ArrayList<>();
+    private ArrayAdapter<String> timeListAdapter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_clock_stopwatch, container, false);
+
         stopwatchTextView = view.findViewById(R.id.clockStopwatchTextView);
         resetStopwatch = view.findViewById(R.id.clockStopwatchReset);
         startStopwatch = view.findViewById(R.id.clockStopwatchStart);
@@ -41,29 +40,30 @@ public class StopwatchFragment extends Fragment {
         timeListAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, timeList);
         lvTimes.setAdapter(timeListAdapter);
 
-        stopwatchManager = new StopwatchManager(stopwatchTextView, new Handler(Looper.getMainLooper()));
+        stopwatchManager = new ViewModelProvider(this).get(StopwatchManager.class);
 
         setupUI();
+        observeStopwatch();
+
         return view;
     }
 
     private void setupUI() {
         stopwatchTextView.setText("00:00:000");
 
-        startStopwatch.setOnClickListener(v -> StartListener());
-        stopStopwatch.setOnClickListener(v -> StopListener());
-        resetStopwatch.setOnClickListener(v -> ResetListener());
+        startStopwatch.setOnClickListener(v -> startStopwatch());
+        stopStopwatch.setOnClickListener(v -> stopStopwatch());
+        resetStopwatch.setOnClickListener(v -> resetStopwatch());
     }
 
-    private void StartListener()
-    {
+    private void startStopwatch() {
         stopwatchManager.start();
         resetStopwatch.setEnabled(false);
         stopStopwatch.setEnabled(true);
         startStopwatch.setEnabled(false);
     }
-    private void StopListener()
-    {
+
+    private void stopStopwatch() {
         stopwatchManager.stop();
         resetStopwatch.setEnabled(true);
         stopStopwatch.setEnabled(false);
@@ -72,10 +72,15 @@ public class StopwatchFragment extends Fragment {
         timeList.add(stopwatchTextView.getText().toString());
         timeListAdapter.notifyDataSetChanged();
     }
-    private void ResetListener()
-    {
+
+    private void resetStopwatch() {
         stopwatchManager.reset();
         stopwatchTextView.setText("00:00:000");
     }
-}
 
+    private void observeStopwatch() {
+        stopwatchManager.getTimeLiveData().observe(getViewLifecycleOwner(), time -> {
+            stopwatchTextView.setText(time);
+        });
+    }
+}
--- MultiPurposeApp/app/src/main/java/com/github/shadeslayer1467/ui/clock/StopwatchManager.java
+++ MultiPurposeApp/app/src/main/java/com/github/shadeslayer1467/ui/clock/StopwatchManager.java
@@ -2,32 +2,37 @@ package com.github.shadeslayer1467.ui.clock;
 
 import android.os.Handler;
 import android.os.SystemClock;
-import android.widget.TextView;
 
-public class StopwatchManager {
-    private final TextView textView;
-    private final Handler handler;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StopwatchManager extends ViewModel {
     private long startTime, timeBuff, updateTime = 0L;
-    private int seconds, minutes, milliSeconds;
     private boolean isRunning = false;
+    private final MutableLiveData<String> timeLiveData = new MutableLiveData<>();
+    private final Handler handler = new Handler();
+
     private final Runnable updater = new Runnable() {
+        @Override
         public void run() {
-            long millisecondTime = SystemClock.uptimeMillis() - startTime;
-            updateTime = timeBuff + millisecondTime;
-            updateTimer();
-            handler.postDelayed(this, 0);
+            if (isRunning) {
+                long millisecondTime = SystemClock.uptimeMillis() - startTime;
+                updateTime = timeBuff + millisecondTime;
+                updateTimer();
+                handler.postDelayed(this, 50);
+            }
         }
     };
 
-    public StopwatchManager(TextView textView, Handler handler) {
-        this.textView = textView;
-        this.handler = handler;
+    public LiveData<String> getTimeLiveData() {
+        return timeLiveData;
     }
 
     public void start() {
         if (!isRunning) {
             startTime = SystemClock.uptimeMillis();
-            handler.postDelayed(updater, 0);
+            handler.post(updater);
             isRunning = true;
         }
     }
@@ -44,19 +49,21 @@ public class StopwatchManager {
         startTime = 0L;
         timeBuff = 0L;
         updateTime = 0L;
-        minutes = 0;
-        seconds = 0;
-        milliSeconds = 0;
-
+        isRunning = false;
         updateTimer();
     }
 
     private void updateTimer() {
-        seconds = (int) (updateTime / 1000);
-        minutes = seconds / 60;
+        int seconds = (int) (updateTime / 1000);
+        int minutes = seconds / 60;
         seconds = seconds % 60;
-        milliSeconds = (int) (updateTime % 1000);
-        textView.setText(String.format("%02d:%02d:%02d", minutes, seconds, milliSeconds));
+        int milliSeconds = (int) (updateTime % 1000);
+        timeLiveData.postValue(String.format("%02d:%02d:%03d", minutes, seconds, milliSeconds));
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        handler.removeCallbacks(updater);
     }
 }
-
